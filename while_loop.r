b9406c3430bba9f610d167c99173de9719e40e3a
Bernmuturi <107024820+ber0622@users.noreply.github.com>
2023-02-17 22:33:10 +0300

    loops

---
+++ while_loop.r
@@ -0,0 +1,24 @@
+#while loop
+#1)infinite loop to avoid
+a=0
+while(a>0)
+{
+print(a)
+a=a+1
+}
+
+#produces first ten even numbers
+a=0
+while(a<20)
+{
+print(a)
+a=a+2
+}
+
+#decr x by 0.5 as long as its positive
+ 
+x=20
+while(x>=0){
+print(x)
+x=x-0.5
+}
