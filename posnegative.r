08f33ebc7ad318fcebe049926bf29d74981bd831
Bernmuturi <107024820+ber0622@users.noreply.github.com>
2023-02-19 23:10:12 +0300

    Update posnegative.r

--- posnegative.r
+++ posnegative.r
@@ -12,3 +12,20 @@ a[i]="negative"
 }
 }
 a
+
+set.seed(123)  # set a random seed for reproducibility
+
+p <- sample(-100:100, 100, replace = TRUE)  # generate 100 values of p between -100 and 100
+
+classif <- rep(NA, length(p))  # create an empty vector to store the classification of the values
+
+for (i in seq_along(p)) {
+  if (p[i] < 0) {
+    classif[i] <- "negative"
+  } else {
+    classif[i] <- "positive"
+  }
+}
+
+print(p)
+print(classif)
